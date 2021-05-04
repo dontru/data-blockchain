9bcbad831665e0c8a0ccc5ca94a48599fb4f8535
dontru <dontru@users.noreply.github.com>
2021-05-04 08:52:54 +0200

    Add Form

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -10,11 +10,29 @@ db = blockchain.DB('Blockchain.SQLite3')
 templates = Jinja2Templates(directory='templates')
 
 
-@app.get("/get/{block_hash}", response_class=HTMLResponse)
-def get(request: Request, block_hash: str):
+@app.get("/", response_class=HTMLResponse)
+def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/get", response_class=HTMLResponse)
+def get(request: Request, block_hash: str = Form(...)):
+    print("get block_hash", block_hash)
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "blockchain": db.get_blockchain(block_hash)
+        "blockchain": db.get_blockchain(block_hash),
+        "block_hash": block_hash,
+    })
+
+
+@app.post("/add", response_class=HTMLResponse)
+def add(request: Request, previous_hash: str = Form(...), data: str = Form(...)):
+    block = db.add_block(previous_hash, data) if previous_hash != "GENESIS" else db.add_genesis(data)
+    block_hash = block.hexdigest()
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "blockchain": db.get_blockchain(block_hash),
+        "block_hash": block_hash,
     })
 
 
